e58f4bf429a360554a50208a4dbaa98b4ddf9172
amiryar <amir.yarmohammady@gmail.com>
2024-10-11 08:39:59 +0400

    reduce size /

--- components/body/home/team/teamcard.js
+++ components/body/home/team/teamcard.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 
 
@@ -6,13 +7,14 @@ export default function Teamcard({ name , describe , image}){
     
     return(
         <div className="">
-                <div className="">
-                    <img src={`data:${image.contentType};base64,${Buffer.from(image.url.data).toString('base64')}`}  alt="colleague" className=" w-full h-[450px]"/>
-                    <div className='btn-card-team'>
-                        <h4 className=" mt-5 font-iransB text-greenD text-2xl">{name}</h4>
-                         <h5 className=" mt-3 text-[#46533B] font-iransB" >{describe}</h5>
-                    </div> 
-                </div>
+                <div className=" relative w-full h-[450px]">
+                    <Image src={`data:${image.contentType};base64,${Buffer.from(image.url.data).toString('base64')}`} fill loading="lazy" quality={75} alt="colleague"/>
+                 </div>
+                <div className='btn-card-team'>
+                    <h4 className=" mt-5 font-iransB text-greenD text-2xl">{name}</h4>
+                     <h5 className=" mt-3 text-[#46533B] font-iransB" >{describe}</h5>
+                </div> 
+                
             
         </div>
 
