1f78305291556e410d2aee06ab25b6d5324c4383
amiryar <amir.yarmohammady@gmail.com>
2024-10-15 23:10:49 +0400

    set title & correcting the lang and dir

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="fa">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>مرکز مشاوره عمومی آفرینش</title>
+      </Head>
+      <body dir="rtl" className="antialiased">
         <Main />
         <NextScript />
       </body>
