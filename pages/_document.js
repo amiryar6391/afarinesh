67f6b56a7ef56021b36b52de8e6289f3a27f445c
amiryar <amir.yarmohammady@gmail.com>
2024-11-22 21:35:17 +0400

    edditing dir

--- pages/_document.js
+++ pages/_document.js
@@ -2,13 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="fa">
+    <Html lang="fa" dir="rtl">
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>مرکز مشاوره عمومی آفرینش</title>
       </Head>
-      <body dir="rtl" className="antialiased">
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
